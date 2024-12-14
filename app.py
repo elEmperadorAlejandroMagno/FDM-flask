8f5464e48ace7197decd03b6383cdd5d91880856
Alejandro Marvich <alejandromarvich181100@gmail.com>
2024-12-14 18:37:00 -0300

    Primer commit del proyecto, primeras configuraciones

---
+++ app.py
@@ -0,0 +1,27 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from utils import get_products
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
+
+db = SQL("sqlite:///fdm.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route('/')
+def hello():
+  PRODUCTS = get_products()
+  return render_template("index.html", products = PRODUCTS)
