0f3fbe2b24a7451dbd77199ace46989bd43eee44
Alejandro Marvich <alejandromarvich181100@gmail.com>
2024-12-18 14:30:21 -0300

    New html templates, estilos css e imagenes nuevas. Nueva ruta product page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from utils.services import get_products_sauce, get_products_merch
+from utils.services import get_products_sauce, get_products_merch, get_product_by_id
 from utils.helpers import uru, login_required
 
 app = Flask(__name__)
@@ -29,3 +29,11 @@ def hello():
   SAUCES = get_products_sauce()
   MERCHANDISING = get_products_merch()
   return render_template("index.html", sauces = SAUCES, merchandising = MERCHANDISING)
+
+@app.route('/product-page')
+def get_product():
+   if request.method == 'GET':
+    id = request.args.get('id')
+    PRODUCT = get_product_by_id(id)
+    return render_template("product.html", details = PRODUCT)
+   
--- utils/services.py
+++ utils/services.py
@@ -26,3 +26,15 @@ def get_products_merch():
     print(f"Data error: {e}")
   return None
 
+def get_product_by_id(id):
+  url = f"http://localhost:1234/products/{id}"
+  try:
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    return data
+  except requests.RequestException as e:
+    print(f"Request error: {e}")
+  except (KeyError, ValueError) as e:
+    print(f"Data error: {e}")
+  return None
