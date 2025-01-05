7a08644bd85f697ec4e665eda2e44815d6978d14
Alejandro Marvich <alejandromarvich181100@gmail.com>
2025-01-05 01:15:21 -0300

    funcitonalidad de cart y cart-page casi terminado. Falta quitar producto

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,4 +1,4 @@
-export function drawCart(cart) {
+export function drawCartModal(cart) {
   const cartTable = document.querySelector('.table-body');
   const CART_ITEM = cart.map(item => `
     <tr class="white-space"></tr>
@@ -21,12 +21,45 @@ export function drawCart(cart) {
     `).join('');
   cartTable.innerHTML = CART_ITEM;
 }
+
+export function drawCartPage(cart) {
+  const cartTable = document.querySelector('.table-body-2');
+  const CART_ITEM = cart.map(item => `
+    <tr class="white-space"></tr>
+      <tr scope="row" class="table-item">
+        <td class="img-td">
+          <img src="${item.image}" alt="${item.title}">
+        </td>
+        <td class="align-top info-item">
+          <h3>${item.title}</h3>
+          <p>${formatCurrency(item.price)}</p>
+        </td>
+        <td class="align-top num-item"><input type="number" value="${item.quantity}"></td>
+        <td class="align-top price-pack">${formatCurrency(item.price * item.quantity)}</td>
+        <td class="align-top">
+          <button class="btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+        </td>
+      </tr>
+      <tr class="white-space"></tr>
+      <tr class="table-group-divider"></tr>
+    `).join('');
+    cartTable.innerHTML = CART_ITEM;
+}
+
 export function formatCurrency(value) {
   return new Intl.NumberFormat('es-UY', {
     style: 'currency',
     currency: 'UYU'
   }).format(value);
 }
+
+export function parseCurrency(value) {
+  if (typeof value === 'number') {
+    return value;
+  }
+  return parseFloat(value.replace(/[^0-9.-]+/g,""));
+}
+
 
 export function drawSubtotal () {
   let subtotal = localStorage.getItem('cartSubtotal', 0);
